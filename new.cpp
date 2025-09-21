a92e11ab8f241244f7d4d3638587abee547ca365
Amna2327 <ahmadalisukhera7849@gmail.com>
2025-09-21 21:25:46 +0500

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main(){
     cout<<"Hello universe"<<endl;
     cout<<"So imma make some changes"<<endl;
+    cout<<"Few more chnages"<<endl;
+    cout<<"Voila"<<endl;
     return 0;
 }

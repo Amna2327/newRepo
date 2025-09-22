382a74e076c28a444fec6d8f3d21e26c5643a52e
Amna Ahmed <ahmadalisukhera7849@gmail.com>
2025-09-22 22:18:13 +0500

    HUHHUHUHU?

--- new.cpp
+++ new.cpp
@@ -4,6 +4,6 @@ int main(){
     cout<<"Hello universe"<<endl;
     cout<<"So imma make some changes"<<endl;
     cout<<"Few more chnages"<<endl;
-    cout<<"DRAMA"<<endl;
+    cout<<"SEQUEL BABEEEEEEEEEEEEEEEEEE"<<endl;
     return 0;
 }
